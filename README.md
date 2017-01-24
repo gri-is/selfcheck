1423afb8c7f95bef673a40fe36e12af98d6e5734
dnoneill <dnoneill>
2017-01-24 10:33:37 -0800

    post modified

---
+++ README.md
@@ -0,0 +1,7 @@
+Alma Self-Check Application
+
+Bugs:
+
+*If wrong user ID is entered the program won't quit
+
+*Metadata shows up differently based on account, won't show checked out books as being checked out but won't actually checkout the book to the account
---
+++ selfcheckout.py
@@ -0,0 +1,32 @@
+from flask import Flask
+app = Flask(__name__)
+from flask import request, json, jsonify
+import requests 
+import xml.etree.ElementTree as ET
+from flask import Response
+
+@app.route('/almaws/v1/users/<userid>&expand=loans,requests,fees&format=json')
+def login(userid):
+	url = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/users/{}".format(userid)
+	params = {}
+	params['apiKey'] = 'l7xx63b4aabaf9264e54a680923760dbfa94'
+	params['expand'] = "loans,requests,fees"
+	params['format'] = "json"
+	response = requests.get(url, params=params)
+	print(response)
+	return Response(response, mimetype="application/json")
+	
+@app.route('/almaws/v1/users/<userid>/loans&user_id_type=all_unique&item_barcode=<barcode>', methods=['GET','POST'])
+def loan(userid, barcode):
+	libraryName = "GC"
+	circDesk = "GRI Open S"
+	url = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/users/{}/loans".format(userid)
+	params = {}
+	params['user_id_type'] = 'all_unique'
+	params['apiKey'] = 'l7xx63b4aabaf9264e54a680923760dbfa94'
+	params['item_barcode'] = barcode
+	headers = {'Content-Type': 'application/xml', 'dataType': "xml"}
+	xml = "<?xml version='1.0' encoding='UTF-8'?><item_loan><circ_desk>%s</circ_desk><library>%s</library></item_loan>" % (circDesk, libraryName)
+	response = requests.post(url, params=params, headers=headers, data=xml)
+	return Response(response, mimetype='xml')
+	
