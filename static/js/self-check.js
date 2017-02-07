69db9132be96b98458f66ade3f5f231ca25f89ae
dnoneill <dnoneill>
2017-02-07 11:29:37 -0800

    removed testing log

--- static/js/self-check.js
+++ static/js/self-check.js
@@ -155,7 +155,7 @@ function loan() {
     	}).fail(function(jqxhr, textStatus, error, status) {
     		//console.log(jqxhr.responseText);
     		console.log(jqxhr.error);
-			console.log(jqxhr);
+	
     		$("#modalheader").text("");
     		if (jqxhr.status == 409) {
     		$("#modalheader").append(jqxhr.responseText + "<br/><br/>See the reference desk for more information<br/><br/><input class='modalclose' type='button' value='close' id='barcodeerrorbutton' onclick='javascript:returnToBarcode();'/>");
