e2669fe62628644f73e4c972e0a1a5d8beea2372
dnoneill <dnoneill>
2017-02-21 09:44:34 -0800

    changed template as requested by Jay

--- static/js/self-check.js
+++ static/js/self-check.js
@@ -142,7 +142,7 @@ function loan() {
 			
 			var value = '';
 			for (var key in data['location_code']) {
-			value += data['location_code'][key] + ' ';
+			value = data['location_code'][key];
 			}
 
     		var templateData = {
